909d12c5b112ec5faf671d763057e7c3ace5687b
ckechlab1 <166001223+ckechlab1@users.noreply.github.com>
2026-05-07 17:47:52 +0000

    list and dict

---
+++ a-datatype.py
@@ -0,0 +1,32 @@
+"""
+dictionary : {}
+{
+    key: value,
+    key1: valu1
+}
+
+"""
+
+d = {
+  "id" : 1234,
+  "name" : "Ck"
+}
+
+print(type(d), d)
+print(d["name"])
+name_of_key = "id"
+print(d[name_of_key])
+
+
+
+# #   0, 1, 2    ,3
+# l=[12,13,"Four",15]
+# print(type(l))
+
+# print("First value:", l[0])
+# print("Last value:", l[-1])
+# print("Last value:", l[3])
+
+# print("Slice arrays:", l[2:-1])
+# print("Slice arrays:", l[2:])
+# print("Slice arrays:", l[0:2])
